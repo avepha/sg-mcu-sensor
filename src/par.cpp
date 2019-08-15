a3fe656150420e99b2450d5af3fcbdde4758f180
fah <farhan.abuhazan@gmail.com>
2019-08-15 10:31:02 +0700

    .

---
+++ src/par.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Alfarie-MBP on 2019-08-15.
+//
+
+#include "par.h"
+
+Par::Par(int pin) : pin(pin) {
+  pinMode(pin, INPUT);
+}
+
+float Par::getPar() {
+  int raw = analogRead(pin);
+  float k = 2.514/1023; // convert to volt (Volt/ADC)
+  float j = 1000; // sensitivity = 1000(Watt/Volt)
+
+  return (float)raw*j*k;
+}
