6111a626485970e94cd5493f52424cfb7235d7f7
fah <farhan.abuhazan@gmail.com>
2019-11-22 23:12:42 +0700

    refactor code

---
+++ cmake-build-all/CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example16_Multitab/file1.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example16_Multitab/file2.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example16_Multitab/ts.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example20_StatusRequest_LTS_WDT_Timeout_Object/SuperSensor.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example21_OO_Callbacks/Calculator.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example21_OO_Callbacks/SuperSensor.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/.pio/libdeps/v1/TaskScheduler_ID721/examples/Scheduler_example21_OO_Callbacks/Ticker.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/external_lib/k30/K30_I2C.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/external_lib/sht1x/SHT1x.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.o"
+  "CMakeFiles/Z_DUMMY_TARGET.dir/src/par.cpp.o"
+  "Z_DUMMY_TARGET"
+  "Z_DUMMY_TARGET.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Z_DUMMY_TARGET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
